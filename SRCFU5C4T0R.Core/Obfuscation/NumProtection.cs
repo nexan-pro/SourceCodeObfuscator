33e89fbe02bb7b3644ee88b6a2839d6dc525f121
0x000N3X4N <0xN3X4N@protonmail.com>
2019-04-18 16:52:13 +0000

    added: expression protection; fixed: declaration protection

--- SRCFU5C4T0R.Core/Obfuscation/NumProtection.cs
+++ SRCFU5C4T0R.Core/Obfuscation/NumProtection.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,13 +18,23 @@ namespace SRCFU5C4T0R.Core.Obfuscation {
 /// </summary>
 class NumProtection {
   private FieldDeclarationSyntax createNewField(FieldDeclarationSyntax field, VariableDeclaratorSyntax fld) {
+    double src_initializer = Convert.ToDouble(field.Declaration.Variables.Last().Initializer.Value.ToString());
+    string encrypted_arg =
+        Convert.ToString(a1f95c1bb38e78c13308
+            .__0xᵉ7ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ362ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(
+                src_initializer));
     FieldDeclarationSyntax tmp = SyntaxFactory.FieldDeclaration(SyntaxFactory.VariableDeclaration( 
                                    SyntaxFactory.ParseTypeName(field.Declaration.Type.ToString()), 
                                      SyntaxFactory.SeparatedList(new[] {
                                        SyntaxFactory.VariableDeclarator(
-                                         SyntaxFactory.Identifier(fld.Identifier.ValueText), null, 
-                                            SyntaxFactory.EqualsValueClause(SyntaxFactory.ParseExpression($"a1f95c1bb38e78c13308.test()"))
+                                         SyntaxFactory.Identifier(fld.Identifier.ValueText), null,
+                                            SyntaxFactory.EqualsValueClause(SyntaxFactory.ParseExpression( (src_initializer % 1 > 0) ?
+                                                $"a1f95c1bb38e78c13308.__0xᵉ1ᵉ333ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ364ᵈᵉᵃᵈᶜᵒᵈᵉᵈ" +
+                                                    $"ᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ({ encrypted_arg })"
+                                            : "a1f95c1bb38e78c13308.__0xᵉ1488ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ("
+                                                + encrypted_arg + ")")
                                          )
+                                        )
                                      })
                                  ));
     
@@ -47,59 +58,83 @@ class NumProtection {
     }
 
     var documents = sln.Projects.SelectMany(x => x.Documents).Select(x => x.Id).ToList();
+    Console.WriteLine($"Count of documents founded is: { documents.Count }");
     foreach (var documentId in documents) {
       List<LocalDeclarationStatementSyntax> vars;
       List<FieldDeclarationSyntax> fields;
       List<AssignmentExpressionSyntax> expr; //TODO: expressions
-      int i = 0; // counters
-      do {
-        var current_doc = sln.GetDocument(documentId);
-        var editor = DocumentEditor.CreateAsync(current_doc).Result;
-        var model = current_doc.GetSemanticModelAsync().Result;
-        var syntax = current_doc.GetSyntaxRootAsync().Result;
-        vars = syntax.DescendantNodes().OfType<LocalDeclarationStatementSyntax>()
-              .Where(x => x.Declaration.Variables.Last().Initializer.Value.Kind() == SyntaxKind.NumericLiteralExpression)
-              .ToList();
-        fields = syntax.DescendantNodes().OfType<FieldDeclarationSyntax>()
-               .Where(x => x.Declaration.Variables.Last().Initializer != null 
-               && x.Declaration.Variables.Last().Initializer.Value.Kind() == SyntaxKind.NumericLiteralExpression)
-               .ToList();
+      
+      Console.WriteLine($"Current document name is: { sln.GetDocument(documentId).Name }");
+      var current_doc = sln.GetDocument(documentId);
+      var editor = DocumentEditor.CreateAsync(current_doc).Result;
+      var syntax = current_doc.GetSyntaxRootAsync().Result;
+      vars = syntax.DescendantNodes().OfType<LocalDeclarationStatementSyntax>()
+            .Where(x => x.Declaration.Variables.Last().Initializer != null && 
+                        x.Declaration.Variables.Last().Initializer.Value.Kind() == SyntaxKind.NumericLiteralExpression)
+            .ToList();
 
-          foreach(var field in fields) {
-            foreach(var fld in field.Declaration.Variables) {
-              Console.WriteLine($"field = {field}");  
-              FieldDeclarationSyntax newField = createNewField(field, fld);
-              Console.WriteLine($"new field is: {newField}");
-              editor.ReplaceNode(fld, newField);
+      fields = syntax.DescendantNodes().OfType<FieldDeclarationSyntax>()
+             .Where(x => x.Declaration.Variables.Last().Initializer != null && 
+                         x.Declaration.Variables.Last().Initializer.Value.Kind() == SyntaxKind.NumericLiteralExpression)
+             .ToList();
+
+      expr = syntax.DescendantNodes().OfType<AssignmentExpressionSyntax>().ToList();
+
+      if (expr.Count > 0) {
+        Console.WriteLine($"[MUTATION] : Count of founded expressions is: {expr.Count}");
+        foreach (var current_expr in expr) {
+          foreach (var expr_node in current_expr.Right.ChildNodes()) {
+            if (expr_node.Kind() == SyntaxKind.NumericLiteralExpression) {
+              double src_token = Convert.ToDouble(expr_node.ToString());
+              double encrypted_arg = a1f95c1bb38e78c13308
+                   .__0xᵉ7ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ362ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(src_token);
+              string formatted_node = (src_token % 1 > 0)
+                    ? "a1f95c1bb38e78c13308.__0xᵉ1ᵉ333ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ364ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ("
+                      + encrypted_arg + ")"
+                    : "a1f95c1bb38e78c13308.__0xᵉ1488ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ("
+                      + encrypted_arg + ")";
+              var new_node = SyntaxFactory.ParseExpression($"{formatted_node}");
+              editor.ReplaceNode(expr_node, new_node);
             }
-            current_doc = editor.GetChangedDocument();
           }
+        }
+      }
 
-          foreach(var var_declorator in vars) {
-            foreach(var vr in var_declorator.Declaration.Variables) {
-              if(vr.Initializer == null) continue;
-              double src_initializer = Convert.ToDouble(vr.Initializer.Value.ToString());
-              string encrypted_arg = Convert.ToString(a1f95c1bb38e78c13308.__0xᵉ7ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ362ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(src_initializer));
-              string formattedInitializer = (src_initializer - Convert.ToInt32(src_initializer) > 0.1) ? 
-                                            "a1f95c1bb38e78c13308.__0xᵉ1ᵉ333ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ364ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(" 
-                                            + encrypted_arg + ");" 
-                                            : "a1f95c1bb38e78c13308.__0xᵉ1488ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ("
-                                            + encrypted_arg + ");";
+      foreach(var field in fields) {
+        foreach(var fld in field.Declaration.Variables) {
+          Console.WriteLine($"[MUTATION]: founded field: { field }");  
+          FieldDeclarationSyntax newField = createNewField(field, fld);
+          Console.WriteLine($"[MUTATION]: new field is: { newField }");
+          editor.ReplaceNode(fld, newField);
+        }
+        current_doc = editor.GetChangedDocument();
+     }
 
-              var newVariable = SyntaxFactory.ParseStatement($"{var_declorator.Declaration.Type} {vr.Identifier} = {formattedInitializer};");
-              newVariable.NormalizeWhitespace();
-              editor.ReplaceNode(vr, newVariable);
-              Console.WriteLine($"[MUTATION] Key: {vr.Identifier} replaced with value: {formattedInitializer}");
-              }
-            current_doc = editor.GetChangedDocument();
-            //Console.WriteLine(current_doc.GetSyntaxRootAsync().Result);
-            i++;
-            }
-          sln = sln.RemoveDocument(documentId);
-          sln = sln.AddDocument(DocumentId.CreateNewId(projId), current_doc.Name, current_doc.GetTextAsync().Result);
-          } while (i < fields.Count);
+     foreach(var var_declorator in vars) {
+       foreach(var vr in var_declorator.Declaration.Variables) {
+         if(vr.Initializer == null) continue;
+           double src_initializer = Convert.ToDouble(vr.Initializer.Value.ToString());
+           string encrypted_arg =
+               Convert.ToString(a1f95c1bb38e78c13308
+                   .__0xᵉ7ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ362ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(
+                       src_initializer));
+           string formattedInitializer = (src_initializer % 1 > 0) ?
+               "a1f95c1bb38e78c13308.__0xᵉ1ᵉ333ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ364ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(" 
+                                         + encrypted_arg + ");"
+               : "a1f95c1bb38e78c13308.__0xᵉ1488ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ("
+                                        + encrypted_arg + ");";
+
+           var newVariable = SyntaxFactory.ParseStatement($"{ var_declorator.Declaration.Type } { vr.Identifier } = { formattedInitializer };");
+           newVariable.NormalizeWhitespace();
+           editor.ReplaceNode(vr, newVariable);
+           Console.WriteLine($"[MUTATION] Key: { vr.Identifier } replaced with value: { formattedInitializer }");
+         }
+       current_doc = editor.GetChangedDocument();
+     }
+     sln = sln.RemoveDocument(documentId);
+     sln = sln.AddDocument(DocumentId.CreateNewId(projId), current_doc.Name, current_doc.GetTextAsync().Result);
     }
     return sln;
   }
-  }
-} 
+}
+}
--- srclib/Helpers/a1f95c1bb38e78c13308.cs
+++ srclib/Helpers/a1f95c1bb38e78c13308.cs
@@ -7,15 +7,15 @@ using System.Threading.Tasks;
 
 namespace srclib.Helpers {
 public static class a1f95c1bb38e78c13308 {
-  [DllImport("BlackLizardPlanetAPI.dll")]
+  [DllImport("BlackLizardPlanetAPI.dll", CallingConvention=CallingConvention.Cdecl)]
   public static extern IntPtr __0x3442496ᵇ96ᵈᵈ01591ᵃ8ᶜᵈ44ᵇ1ᵉᵉᶜ1368ᵃᵇ728ᵃᵇᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(string data);
-  [DllImport("BlackLizardPlanetAPI.dll")]
+  [DllImport("BlackLizardPlanetAPI.dll", CallingConvention=CallingConvention.Cdecl)]
   public static extern IntPtr __0xᵉ7ᵉ371ᶠ3ᶠ8ᵃᵃ56895ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ362ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(string data);
-  [DllImport("BlackLizardPlanetAPI.dll")]
+  [DllImport("BlackLizardPlanetAPI.dll", CallingConvention=CallingConvention.Cdecl)]
   public static extern double __0xᵉ7ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ362ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(double encr_n);
-  [DllImport("BlackLizardPlanetAPI.dll")]
+  [DllImport("BlackLizardPlanetAPI.dll", CallingConvention=CallingConvention.Cdecl)]
   public static extern double __0xᵉ1ᵉ333ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃ364ᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(double decr_n);
-  [DllImport("BlackLizardPlanetAPI.dll")]
+  [DllImport("BlackLizardPlanetAPI.dll", CallingConvention=CallingConvention.Cdecl)]
   public static extern int __0xᵉ1488ᵉ371ᶠ3ᶠ8ᵃᵃ13212ᵉ9ᵃᵈ4790ᵃ53432ᶠ2ᵈᵇᶜᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ(int decr_n);
 
   public static string __0x3442496ᵇ96ᵈᵈ01591ᵃ8ᶜᵈ44ᵇ1ᵉᵉᶜ1368ᵃᵇ728ᵃᵇᵃᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉᵈᵉᵃᵈᶜᵒᵈᵉ_(string encr) {
